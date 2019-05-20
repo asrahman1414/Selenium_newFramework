30ae762680a42d754f77601958b848ef399b2b7f
golam <asrahman445@gmail.com>
2019-05-20 14:23:21 -0400

    This is a framework for selenium that has been created with many options

---
+++ src/main/java/org/apache/log4j/PropertyConfigurator.java
@@ -0,0 +1,20 @@
+package org.apache.log4j;
+
+public class PropertyConfigurator {
+
+	public static void configure11(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void configure1(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void configure(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
